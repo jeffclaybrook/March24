be0d693fc42ccc6848b75c15d2ffa190c7c3fa38
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2022-03-25 08:36:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,157 +2,157 @@ let songs = [
    {
       name: 'Sherane a.k.a Master Splinters Daughter',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-1.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-1.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-1.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-1.webp'
    },
    {
       name: 'Bitch, Dont Kill My Vibe',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-2.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-2.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-2.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-2.webp'
    },
    {
       name: 'Backseat Freestyle',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-3.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-3.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-3.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-3.webp'
    },
    {
       name: 'The Art of Peer Pressure',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-4.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-4.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-4.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-4.webp'
    },
    {
       name: 'Money Trees (feat. Jay Rock)',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-5.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-5.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-5.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-5.webp'
    },
    {
       name: 'Poetic Justice (feat. Drake)',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-6.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-6.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-6.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-6.webp'
    },
    {
       name: 'Good Kid',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-7.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-7.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-7.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-7.webp'
    },
    {
       name: 'm.A.A.d city (feat. MC Eiht)',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-8.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-8.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-8.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-8.webp'
    },
    {
       name: 'Swimming Pools (Drank) [Extended Version]',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-9.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-9.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-9.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-9.webp'
    },
    {
       name: 'Sing About Me, Im Dying of Thirst',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-10.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-10.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-10.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-10.webp'
    },
    {
       name: 'Real (feat. Anna Wise)',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-11.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-11.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-11.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-11.webp'
    },
    {
       name: 'Compton (feat. Dr. Dre)',
       artist: 'Kendrick Lamar',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-12.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-12.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-12.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-12.webp'
    },
    {
       name: 'All We Got (feat. Kanye West & Chicago Childrens Choir)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-13.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-13.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-13.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-13.webp'
    },
    {
       name: 'No Problem (feat. Lil Wayne & 2 Chainz)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-14.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-14.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-14.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-14.webp'
    },
    {
       name: 'Summer Friends (feat. Jeremih & Francis & The Lights)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-15.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-15.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-15.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-15.webp'
    },
    {
       name: 'D.R.A.M. Sings Special',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-16.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-16.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-16.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-16.webp'
    },
    {
       name: 'Blessings (feat. Jamila Woods)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-17.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-17.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-17.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-17.webp'
    },
    {
       name: 'Same Drugs',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-18.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-18.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-18.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-18.webp'
    },
    {
       name: 'Mixtape (feat. Young Thug & Lil Yachty)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-19.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-19.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-19.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-19.webp'
    },
    {
       name: 'Angels (feat. Saba)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-20.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-20.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-20.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-20.webp'
    },
    {
       name: 'Juke Jam (feat. Justin Bieber & Towkio)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-21.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-21.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-21.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-21.webp'
    },
    {
       name: 'All Night (feat. Knox Fortune)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-22.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-22.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-22.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-22.webp'
    },
    {
       name: 'How Great (feat. Jay Electronica & My cousin Nicole)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-23.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-23.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-23.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-23.webp'
    },
    {
       name: 'Smoke Break (feat. Future)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-24.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-24.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-24.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-24.webp'
    },
    {
       name: 'Finish Line / Drown (feat. T-Pain, Kirk Franklin, Eryn Allen Kane & Noname)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-25.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-25.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-25.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-25.webp'
    },
    {
       name: 'Blessings (feat. Ty Dolla $ign, Raury, BJ The Chicago Kid & Anderson .Paak)',
       artist: 'Chance the Rapper',
-      path: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/songs/song-26.mp3',
-      cover: 'https://raw.githubusercontent.com/jeffclaybrook/assets/main/images/image-26.webp'
+      path: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/songs/song-26.mp3',
+      cover: 'https://raw.githubusercontent.com/jeffclaybrook/March24/main/images/image-26.webp'
    }
 ]
